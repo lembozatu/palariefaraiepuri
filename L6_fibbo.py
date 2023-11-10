5816c4b2547dcdd51dee518a0456df5402438179
lembozatu <86881663+lembozatu@users.noreply.github.com>
2023-11-10 09:02:14 +0200

    Update L6_fibbo.py

--- L6_fibbo.py
+++ L6_fibbo.py
@@ -1,10 +1,12 @@
 sir =[0,1]
 numar=int(input("Introdu numarul:"))
-if numar==2:
-    print(sir)
-elif numar==1:
-    print(sir[0])
-else:
-    for i in range(numar-2):
-            sir.append(sir[i]+sir[i+1])
-    print(sir)
+def fib(numar):
+    if numar==2:
+        print(sir)
+    elif numar==1:
+        print(sir[0])
+    else:
+        for i in range(numar-2):
+                sir.append(sir[i]+sir[i+1])
+        print(sir)
+fib(numar)
